d00ecd20fa8f7605986b622327a328c6728d1f95
Konata Izumi <a@2kx.ru>
2021-03-23 20:53:01 +0300

    remove keycloak from project; add simple auth;

--- src/main/java/application/configurations/SecurityConfig.kt
+++ src/main/java/application/configurations/SecurityConfig.kt
@@ -4,6 +4,16 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.security.core.userdetails.User
+import java.io.File
+import java.io.IOError
+import java.io.IOException
+import java.nio.file.Files
+import kotlin.random.Random
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +25,37 @@ open class SecurityConfig : WebSecurityConfigurerAdapter() {
                 .disable()
                 .cors()
                 .and()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic()
+    }
+
+    @Autowired
+    @Throws(java.lang.Exception::class)
+    open fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        try {
+            val authx = auth.inMemoryAuthentication()
+            val f = File("/config/auth.conf")
+            if (f.exists() && f.isFile && f.canRead()) {
+                var dataLines = Files.readAllLines(File("/config/auth.conf").toPath())
+                if (dataLines == null || dataLines.isEmpty()) {
+                    throw IOException()
+                }
+                dataLines.forEach{
+                    val data = it.trim().split(":")
+                    if (data.size > 2) {
+                        val user = User.withUsername(data[0]).password(data[1]).roles(data[2]).passwordEncoder { pass -> pass }.build()
+                        authx.withUser(user)
+                    }
+                }
+            } else {
+                throw IOException()
+            }
+        } catch (e : Exception) {
+            auth.inMemoryAuthentication()
+                    .withUser("admin+" + System.nanoTime() + Random.nextLong())
+                    .password("admin+" + System.nanoTime() + Random.nextLong())
+                    .roles("USER")
+        }
     }
 }
